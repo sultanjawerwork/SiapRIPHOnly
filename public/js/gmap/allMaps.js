cee6e3515616c71db4151021b080c032a0ec4bbc
sultanjawer <sultanjawer@gmail.com>
2023-06-07 08:03:47 +0700

    penyempurnaan peta

--- public/js/gmap/allMaps.js
+++ public/js/gmap/allMaps.js
@@ -1,3 +1,8 @@
+// Declare global variables
+var map;
+var markers = [];
+
+// Initialize the map
 function initMap() {
 	map = new google.maps.Map(document.getElementById("allMap"), {
 		center: { lat: -2.548926, lng: 118.014863 },
@@ -5,198 +10,227 @@ function initMap() {
 		mapTypeId: google.maps.MapTypeId.HYBRID,
 	});
 
-	// Make an Ajax request to retrieve the marker data and polygons
+	// Handle the change event of #periodetahun element
 	$("#periodetahun").on("change", function () {
-		initMap();
-
 		var periodetahun = $(this).val();
 		var url =
-			periodetahun == "all"
+			periodetahun === "all"
 				? "http://127.0.0.1:8000/api/getAPIAnggotaMitraAll/"
 				: "http://127.0.0.1:8000/api/getAPIAnggotaMitraByYear/" + periodetahun;
+
+		// Make an AJAX request to retrieve marker data and polygons
 		$.ajax({
 			url: url,
 			type: "GET",
 			dataType: "json",
 			success: function (data) {
-				$.each(data, function (index, anggotaMitra) {
-					if (anggotaMitra.latitude && anggotaMitra.longitude) {
-						var marker = new google.maps.Marker({
-							position: {
-								lat: parseFloat(anggotaMitra.latitude),
-								lng: parseFloat(anggotaMitra.longitude),
-							},
-							map: map,
-							id: anggotaMitra.id,
-							npwp: anggotaMitra.npwp,
-							periodetahun: anggotaMitra.periodetahun,
-							latitude: anggotaMitra.latitude,
-							longitude: anggotaMitra.longitude,
-							no_ijin: anggotaMitra.no_ijin,
-							no_perjanjian: anggotaMitra.no_perjanjian,
-							nama_lokasi: anggotaMitra.nama_lokasi,
-							panen_pict: anggotaMitra.panen_pict,
-							tanam_pict: anggotaMitra.tanam_pict,
-
-							nama_petani: anggotaMitra.nama_petani,
-							nama_kelompok: anggotaMitra.nama_kelompok,
-							nama_lokasi: anggotaMitra.nama_lokasi,
-
-							altitude: anggotaMitra.altitude,
-							luas_kira: anggotaMitra.luas_kira,
-							tgl_tanam: anggotaMitra.tgl_tanam,
-							luas_tanam: anggotaMitra.luas_tanam,
-							varietas: anggotaMitra.varietas,
-							tgl_panen: anggotaMitra.tgl_panen,
-							volume: anggotaMitra.volume,
-
-							company: anggotaMitra.company,
-						});
-
-						marker.addListener("click", function () {
-							map.setZoom(15);
-							map.panTo(marker.getPosition());
-
-							// Send an AJAX request to get the marker data
-
-							$.ajax({
-								url:
-									"http://127.0.0.1:8000/api/getAPIAnggotaMitra/" +
-									anggotaMitra.id,
-								type: "GET",
-								dataType: "json",
-								success: function (data) {
-									// Create a string containing the marker data
-									var markerId = marker.id;
-									var npwp = marker.npwp;
-									var no_ijin = marker.no_ijin;
-									var periodetahun = marker.periodetahun;
-									var no_perjanjian = marker.no_perjanjian;
-									var nama_lokasi = marker.nama_lokasi;
-									var panenPictName = marker.panen_pict;
-									var panenPict = marker.panen_pict;
-									var tanamPictName = marker.tanam_pict;
-									var tanamPict = marker.tanam_pict;
-
-									var nama_petani = marker.nama_petani;
-									var nama_kelompok = marker.nama_kelompok;
-									var nama_lokasi = marker.nama_lokasi;
-									var altitude = marker.altitude;
-									var luas_kira = marker.luas_kira;
-									var tgl_tanam = marker.tgl_tanam;
-									var luas_tanam = marker.luas_tanam;
-									var varietas = marker.varietas;
-									var tgl_panen = marker.tgl_panen;
-									var volume = marker.volume;
-
-									var company = marker.company;
-
-									// Set the modal content to the marker details
-									$("#markerModal #markerId").text(markerId);
-									$("#markerModal #no_ijin").text(no_ijin);
-									$("#markerModal #no_perjanjian").text(no_perjanjian);
-									$("#markerModal #nama_lokasi").text(nama_lokasi);
-									$("#markerModal #npwp").text(npwp);
-									$("#markerModal #company").text(company);
-
-									//set the <a> element for panen
-
-									$("#markerModal #panenPictName").html(
-										`<a href="/storage/uploads/${npwp}/${periodetahun}/${panenPict}" target="_blank">${panenPictName}</a>`
-									);
-									$("#markerModal #panenPict").attr(
-										"src",
-										"/storage/uploads/" +
-											npwp +
-											"/" +
-											periodetahun +
-											"/" +
-											panenPict
-									);
-									$("#markerModal #panenPict")
-										.parent("a")
-										.attr(
-											"href",
-											"/storage/uploads/" +
-												npwp +
-												"/" +
-												periodetahun +
-												"/" +
-												panenPict
-										);
-
-									//set the <a> element for tanam
-									$("markerModal #tanamPictName").html(
-										`<a href="/storage/uploads/${npwp}/${periodetahun}/${tanamPict}" target="_blank">${tanamPictName}</a>`
-									);
-									$("#markerModal #tanamPict").attr(
-										"src",
-										"/storage/uploads/" +
-											npwp +
-											"/" +
-											periodetahun +
-											"/" +
-											tanamPict
-									);
-
-									$("#markerModal #tanamPict")
-										.parent("a")
-										.attr(
-											"href",
-											"/storage/uploads/" +
-												npwp +
-												"/" +
-												periodetahun +
-												"/" +
-												tanamPict
-										);
-									// $("#markerModal #tanamPictName").text(tanamPictName);
-									// $("#markerModal #tanamPict").attr(
-									// 	"src",
-									// 	"/storage/docs/pks/anggota/tanam/img/" + tanamPict
-									// );
-
-									$("#markerModal #nama_petani").text(nama_petani);
-									$("#markerModal #nama_kelompok").text(nama_kelompok);
-									$("#markerModal #nama_lokasi").text(nama_lokasi);
-
-									$("#markerModal #altitude").text(altitude);
-									$("#markerModal #luas_kira").text(luas_kira);
-									$("#markerModal #tgl_tanam").text(tgl_tanam);
-									$("#markerModal #luas_tanam").text(luas_tanam);
-									$("#markerModal #varietas").text(varietas);
-									$("#markerModal #tgl_panen").text(tgl_panen);
-									$("#markerModal #volume").text(volume);
-									console.log(npwp);
-									// Show the modal
-									$("#markerModal").modal("show");
-								},
-							});
-						});
-					}
-
-					if (anggotaMitra.polygon) {
-						var polygon = new google.maps.Polygon({
-							paths: JSON.parse(anggotaMitra.polygon),
-							strokeColor: "#FF0000",
-							strokeOpacity: 0.8,
-							strokeWeight: 2,
-							fillColor: "#FF0000",
-							fillOpacity: 0.35,
-							map: map,
-						});
-
-						polygon.addListener("click", function () {
-							var bounds = new google.maps.LatLngBounds();
-							polygon.getPath().forEach(function (latLng) {
-								bounds.extend(latLng);
-							});
-							map.fitBounds(bounds);
-						});
-					}
-				});
+				handleMarkerData(data);
 			},
 		});
+	});
+}
+
+// Handle marker data and create markers and polygons
+function handleMarkerData(data) {
+	// Remove existing markers and polygons from the map
+	removeMarkers();
+	removePolygons();
+
+	// Iterate over the data to create markers and polygons
+	$.each(data, function (index, anggotaMitra) {
+		if (anggotaMitra.latitude && anggotaMitra.longitude) {
+			createMarker(anggotaMitra);
+		}
+
+		if (anggotaMitra.polygon) {
+			createPolygon(anggotaMitra);
+		}
+	});
+
+	// Check for polygon intersections
+	var intersections = map.findPolygonIntersections();
+	if (intersections.length > 0) {
+		console.log("There are intersections between polygons!");
+		// Perform additional actions for intersecting polygons
+		// You can access the intersecting polygons using intersections array
+	}
+}
+
+// Create a marker on the map
+function createMarker(anggotaMitra) {
+	var marker = new google.maps.Marker({
+		position: {
+			lat: parseFloat(anggotaMitra.latitude),
+			lng: parseFloat(anggotaMitra.longitude),
+		},
+		map: map,
+		// Set other properties of the marker here
+	});
+	// Assign the id property from anggotaMitra to the marker object
+	marker.id = anggotaMitra.id;
+	marker.npwp = anggotaMitra.npwp;
+	marker.periodetahun = anggotaMitra.periodetahun;
+	marker.latitude = anggotaMitra.latitude;
+	marker.longitude = anggotaMitra.longitude;
+	marker.no_ijin = anggotaMitra.no_ijin;
+	marker.no_perjanjian = anggotaMitra.no_perjanjian;
+	marker.nama_lokasi = anggotaMitra.nama_lokasi;
+	marker.panen_pict = anggotaMitra.panen_pict;
+	marker.tanam_pict = anggotaMitra.tanam_pict;
+
+	marker.nama_petani = anggotaMitra.nama_petani;
+	marker.nama_kelompok = anggotaMitra.nama_kelompok;
+	marker.nama_lokasi = anggotaMitra.nama_lokasi;
+
+	marker.altitude = anggotaMitra.altitude;
+	marker.luas_kira = anggotaMitra.luas_kira;
+	marker.tgl_tanam = anggotaMitra.tgl_tanam;
+	marker.luas_tanam = anggotaMitra.luas_tanam;
+	marker.varietas = anggotaMitra.varietas;
+	marker.tgl_panen = anggotaMitra.tgl_panen;
+	marker.volume = anggotaMitra.volume;
+
+	marker.company = anggotaMitra.company;
+
+	// Add a click event listener to the marker
+	marker.addListener("click", function () {
+		showMarkerDetails(marker);
+	});
+}
+
+// Create a polygon on the map
+function createPolygon(anggotaMitra) {
+	var polygon = new google.maps.Polygon({
+		paths: JSON.parse(anggotaMitra.polygon),
+		strokeColor: "#FF0000",
+		strokeOpacity: 0.8,
+		strokeWeight: 2,
+		fillColor: "#FF0000",
+		fillOpacity: 0.35,
+		map: map,
+	});
+
+	// Add a click event listener to the polygon
+	polygon.addListener("click", function () {
+		zoomToPolygon(polygon);
+	});
+}
+
+// Show marker details in a modal
+function showMarkerDetails(marker) {
+	// Send an AJAX request to get the marker data
+	$.ajax({
+		url: "http://127.0.0.1:8000/api/getAPIAnggotaMitra/" + marker.id,
+		type: "GET",
+		dataType: "json",
+		success: function (data) {
+			// Set the modal content with the marker details
+			var markerId = marker.id;
+			var npwp = marker.npwp;
+			var no_ijin = marker.no_ijin;
+			var periodetahun = marker.periodetahun;
+			var no_perjanjian = marker.no_perjanjian;
+			var nama_lokasi = marker.nama_lokasi;
+			var panenPictName = marker.panen_pict;
+			var panenPict = marker.panen_pict;
+			var tanamPictName = marker.tanam_pict;
+			var tanamPict = marker.tanam_pict;
+
+			var nama_petani = marker.nama_petani;
+			var nama_kelompok = marker.nama_kelompok;
+			var altitude = marker.altitude;
+			var luas_kira = marker.luas_kira;
+			var tgl_tanam = marker.tgl_tanam;
+			var luas_tanam = marker.luas_tanam;
+			var varietas = marker.varietas;
+			var tgl_panen = marker.tgl_panen;
+			var volume = marker.volume;
+
+			var company = marker.company;
+
+			// Update the modal elements with the marker data
+			$("#markerModal #markerId").text(markerId);
+			$("#markerModal #no_ijin").text(no_ijin);
+			$("#markerModal #no_perjanjian").text(no_perjanjian);
+			$("#markerModal #nama_lokasi").text(nama_lokasi);
+			$("#markerModal #npwp").text(npwp);
+			$("#markerModal #company").text(company);
+
+			//set the <a> element for panen
+			$("#markerModal #panenPictName").html(
+				`<a href="/storage/uploads/${npwp}/${periodetahun}/${panenPict}" target="_blank">${panenPictName}</a>`
+			);
+			$("#markerModal #panenPict").attr(
+				"src",
+				"/storage/uploads/" + npwp + "/" + periodetahun + "/" + panenPict
+			);
+			$("#markerModal #panenPict")
+				.parent("a")
+				.attr(
+					"href",
+					"/storage/uploads/" + npwp + "/" + periodetahun + "/" + panenPict
+				);
+
+			//set the <a> element for tanam
+			$("#markerModal #tanamPictName").html(
+				`<a href="/storage/uploads/${npwp}/${periodetahun}/${tanamPict}" target="_blank">${tanamPictName}</a>`
+			);
+			$("#markerModal #tanamPict").attr(
+				"src",
+				"/storage/uploads/" + npwp + "/" + periodetahun + "/" + tanamPict
+			);
+
+			$("#markerModal #tanamPict")
+				.parent("a")
+				.attr(
+					"href",
+					"/storage/uploads/" + npwp + "/" + periodetahun + "/" + tanamPict
+				);
+
+			$("#markerModal #nama_petani").text(nama_petani);
+			$("#markerModal #nama_kelompok").text(nama_kelompok);
+			$("#markerModal #nama_lokasi").text(nama_lokasi);
+			$("#markerModal #altitude").text(altitude);
+			$("#markerModal #luas_kira").text(luas_kira);
+			$("#markerModal #tgl_tanam").text(tgl_tanam);
+			$("#markerModal #luas_tanam").text(luas_tanam);
+			$("#markerModal #varietas").text(varietas);
+			$("#markerModal #tgl_panen").text(tgl_panen);
+			$("#markerModal #volume").text(volume);
+
+			// Show the modal
+			$("#markerModal").modal("show");
+			zoomToMarker(marker);
+		},
+	});
+}
+
+// Zoom the map to fit the marker
+function zoomToMarker(marker) {
+	map.setZoom(18);
+	map.setCenter(marker.getPosition());
+}
+
+// Zoom the map to fit the polygon bounds
+function zoomToPolygon(polygon) {
+	var bounds = new google.maps.LatLngBounds();
+	polygon.getPath().forEach(function (latLng) {
+		bounds.extend(latLng);
+	});
+	map.fitBounds(bounds);
+}
+
+// Remove all markers from the map
+function removeMarkers() {
+	map.getMarkers().forEach(function (marker) {
+		marker.setMap(null);
+	});
+}
+
+// Remove all polygons from the map
+function removePolygons() {
+	map.getPolygons().forEach(function (polygon) {
+		polygon.setMap(null);
 	});
 }
 
@@ -208,28 +242,6 @@ google.maps.Map.prototype.getMarkers = function () {
 		if (overlay instanceof google.maps.Marker) {
 			markers.push(overlay);
 		} else if (overlay instanceof google.maps.Polygon) {
-			// If the overlay is a polygon, iterate over its paths and add any markers to the list
-			overlay.getPath().forEach(function (path) {
-				if (path instanceof google.maps.Marker) {
-					markers.push(path);
-				}
-			});
-		}
-	}
-	return markers;
-};
-
-// Extend the Map object to add a
-
-// Extend the Map object to add a getMarkers() method
-google.maps.Map.prototype.getMarkers = function () {
-	var markers = [];
-	for (var i = 0; i < this.overlayMapTypes.length; i++) {
-		var overlay = this.overlayMapTypes.getAt(i);
-		if (overlay instanceof google.maps.Marker) {
-			markers.push(overlay);
-		} else if (overlay instanceof google.maps.Polygon) {
-			// If the overlay is a polygon, iterate over its paths and add any markers to the list
 			overlay.getPath().forEach(function (path) {
 				if (path instanceof google.maps.Marker) {
 					markers.push(path);
@@ -251,4 +263,6 @@ google.maps.Map.prototype.getPolygons = function () {
 	}
 	return polygons;
 };
+
+// Call the initMap function to initialize the map
 initMap();
--- public/js/gmap/map.js
+++ public/js/gmap/map.js
@@ -1,28 +1,22 @@
-// map.js
-
-// 1. create a new map 'myMap' with initial
-// - center: { lat: -2.5489, lng: 118.0149 },
-// - zoom: 5,
+// refactored map.js
 let myMap;
+const markers = [];
+const polygons = [];
 
 function initMap() {
-	//init the map viewport
 	myMap = new google.maps.Map(document.getElementById("myMap"), {
 		center: { lat: -2.5489, lng: 118.0149 },
 		zoom: 5,
 		mapTypeId: google.maps.MapTypeId.SATELLITE,
 	});
 
-	// 2. array to store the marker and polygon
-	const markers = [];
-	const polygons = [];
+	setupDrawingManager();
+	createMarkers();
+	createPolygon();
+}
 
-	// var infowindow = new google.maps.InfoWindow({
-	// 	content: "Hello World!",
-	// });
-
-	// 3. setup drawing manager
-	var drawingManager = new google.maps.drawing.DrawingManager({
+function setupDrawingManager() {
+	const drawingManager = new google.maps.drawing.DrawingManager({
 		drawingMode: google.maps.drawing.OverlayType.DEFAULT,
 		drawingControl: true,
 		drawingControlOptions: {
@@ -38,69 +32,66 @@ function initMap() {
 			draggable: true,
 		},
 	});
-	drawingManager.setMap(myMap); //attach draw manager to map
-
-	/** 3. event listener for when a marker is completed
-	 *
-	 * add the marker to the array of markers and make it draggable
-	 * add a listener for when the marker is clicked, center the map on the marker, and update the input fields
-	 * add a listener for when the marker is dragged, update the input fields
-	 */
+	drawingManager.setMap(myMap);
 
 	google.maps.event.addListener(
 		drawingManager,
 		"markercomplete",
 		function (marker) {
-			// add the marker to the array of markers and make it draggable
 			markers.push(marker);
 			marker.setDraggable(true);
-			//  add a listener for when the marker is clicked, center the map on the marker, and update the input fields
-			google.maps.event.addListener(marker, "click", function () {
-				myMap.setCenter(marker.getPosition());
-				document.getElementById("latitude").value = marker.getPosition().lat();
-				document.getElementById("longitude").value = marker.getPosition().lng();
-			});
-			//  add a listener for when the marker is dragged, update the input fields
-			google.maps.event.addListener(marker, "drag", function () {
-				document.getElementById("latitude").value = marker.getPosition().lat();
-				document.getElementById("longitude").value = marker.getPosition().lng();
-			});
+			addMarkerListeners(marker);
 		}
 	);
 
-	/** 4. check if the latitude and longitude inputs have values
-	 *
-	 * and if true, create a marker at that position
-	 * add marker
-	 */
-	// check if the latitude and longitude inputs have values, and if so, create a marker at that position
-	var latitude = document.getElementById("latitude").value;
-	var longitude = document.getElementById("longitude").value;
-	if (latitude != "" && longitude != "") {
-		var position = new google.maps.LatLng(latitude, longitude);
-		var marker = new google.maps.Marker({
+	google.maps.event.addListener(
+		drawingManager,
+		"polygoncomplete",
+		function (polygon) {
+			polygons.push(polygon);
+			addPolygonListeners(polygon);
+		}
+	);
+}
+
+function addMarkerListeners(marker) {
+	google.maps.event.addListener(marker, "click", function () {
+		myMap.setCenter(marker.getPosition());
+		updateCoordinates(marker.getPosition());
+	});
+
+	google.maps.event.addListener(marker, "drag", function () {
+		updateCoordinates(marker.getPosition());
+	});
+}
+
+function updateCoordinates(position) {
+	document.getElementById("latitude").value = position.lat();
+	document.getElementById("longitude").value = position.lng();
+}
+
+function createMarkers() {
+	const latitude = document.getElementById("latitude").value;
+	const longitude = document.getElementById("longitude").value;
+	if (latitude !== "" && longitude !== "") {
+		const position = new google.maps.LatLng(latitude, longitude);
+		const marker = new google.maps.Marker({
 			position: position,
 			map: myMap,
 			draggable: true,
 		});
-		// add the marker to the array of markers, center the map on the marker, and add listeners for click and drag events
 		markers.push(marker);
 		myMap.setCenter(position);
 		myMap.setZoom(18);
-		google.maps.event.addListener(marker, "click", function () {
-			myMap.setCenter(marker.getPosition());
-		});
-		google.maps.event.addListener(marker, "drag", function () {
-			document.getElementById("latitude").value = marker.getPosition().lat();
-			document.getElementById("longitude").value = marker.getPosition().lng();
-		});
+		addMarkerListeners(marker);
 	}
+}
 
-	// 5. check if polygon has input value
-	var polygonCoords = document.getElementById("polygon").value;
-	if (polygonCoords != "") {
+function createPolygon() {
+	const polygonCoords = document.getElementById("polygon").value;
+	if (polygonCoords !== "") {
 		polygonCoords = JSON.parse(polygonCoords);
-		var polygon = new google.maps.Polygon({
+		const polygon = new google.maps.Polygon({
 			paths: polygonCoords,
 			strokeColor: "#FF0000",
 			strokeOpacity: 0.8,
@@ -110,82 +101,35 @@ function initMap() {
 			editable: true,
 			map: myMap,
 		});
-		// add a listener for when the polygon is clicked, and update the input field
-		google.maps.event.addListener(polygon, "click", function () {
-			document.getElementById("polygon").value = JSON.stringify(
-				polygon.getPath().getArray()
-			);
-		});
-		// add a listener for when the polygon is edited, and update the input field
-		google.maps.event.addListener(polygon.getPath(), "set_at", function () {
-			document.getElementById("polygon").value = JSON.stringify(
-				polygon.getPath().getArray()
-			);
-			// calculate and display the area of the polygon
-			var luas = google.maps.geometry.spherical.computeArea(polygon.getPath());
-			document.getElementById("luas_kira").value = (luas / 10000).toFixed(2);
-		});
-		google.maps.event.addListener(polygon.getPath(), "insert_at", function () {
-			document.getElementById("polygon").value = JSON.stringify(
-				polygon.getPath().getArray()
-			);
-			// calculate and display the area of the polygon
-			var luas = google.maps.geometry.spherical.computeArea(polygon.getPath());
-			document.getElementById("luas_kira").value = (luas / 10000).toFixed(2);
-		});
+		polygons.push(polygon);
+		addPolygonListeners(polygon);
 	}
+}
 
-	// Part 9 add a listener for when a polygon is completed
-	google.maps.event.addListener(
-		drawingManager,
-		"polygoncomplete",
-		function (polygon) {
-			// add the polygon to the map
-			polygon.setMap(myMap);
+function addPolygonListeners(polygon) {
+	google.maps.event.addListener(polygon, "click", function () {
+		document.getElementById("polygon").value = JSON.stringify(
+			polygon.getPath().getArray()
+		);
+		calculatePolygonArea(polygon);
+	});
 
-			// add a listener for when the polygon is clicked, and update the input field
-			google.maps.event.addListener(polygon, "click", function () {
-				document.getElementById("polygon").value = JSON.stringify(
-					polygon.getPath().getArray()
-				);
+	google.maps.event.addListener(polygon.getPath(), "set_at", function () {
+		document.getElementById("polygon").value = JSON.stringify(
+			polygon.getPath().getArray()
+		);
+		calculatePolygonArea(polygon);
+	});
 
-				// calculate and display the area of the polygon
-				var luas = google.maps.geometry.spherical.computeArea(
-					polygon.getPath()
-				);
-				document.getElementById("luas_kira").value = (luas / 10000).toFixed(2);
-			});
+	google.maps.event.addListener(polygon.getPath(), "insert_at", function () {
+		document.getElementById("polygon").value = JSON.stringify(
+			polygon.getPath().getArray()
+		);
+		calculatePolygonArea(polygon);
+	});
+}
 
-			// add a listener for when the polygon is edited, and update the input field
-			google.maps.event.addListener(polygon.getPath(), "set_at", function () {
-				document.getElementById("polygon").value = JSON.stringify(
-					polygon.getPath().getArray()
-				);
-
-				// calculate and display the area of the polygon
-				var luas = google.maps.geometry.spherical.computeArea(
-					polygon.getPath()
-				);
-				document.getElementById("luas_kira").value = (luas / 10000).toFixed(2);
-			});
-
-			google.maps.event.addListener(
-				polygon.getPath(),
-				"insert_at",
-				function () {
-					document.getElementById("polygon").value = JSON.stringify(
-						polygon.getPath().getArray()
-					);
-
-					// calculate and display the area of the polygon
-					var luas = google.maps.geometry.spherical.computeArea(
-						polygon.getPath()
-					);
-					document.getElementById("luas_kira").value = (luas / 10000).toFixed(
-						2
-					);
-				}
-			);
-		}
-	);
+function calculatePolygonArea(polygon) {
+	const luas = google.maps.geometry.spherical.computeArea(polygon.getPath());
+	document.getElementById("luas_kira").value = (luas / 10000).toFixed(2);
 }
