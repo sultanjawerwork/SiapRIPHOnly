12a10266beee432e209100d64c9d480295afe180
sultanjawer <sultanjawer@gmail.com>
2023-06-21 23:15:33 +0700

    migration API user

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -7,38 +7,47 @@ use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
 {
-    public function run()
-    {
-        $users = [
-            [
-                'id'             => 1,
-                'name'           => 'Admin',
-                'email'          => 'admin@admin.com',
-                'password'       => bcrypt('password'),
-                'remember_token' => null,
-                'username'       => 'admin',
-                'roleaccess'     => 1,
-            ],
-            [
-                'id'             => 2,
-                'name'           => 'User1',
-                'email'          => 'user@user.com',
-                'password'       => bcrypt('password'),
-                'remember_token' => null,
-                'username'       => 'user1',
-                'roleaccess'     => 2,
-            ],
-            [
-                'id'             => 3,
-                'name'           => 'Andi Muhammad Idil Fitri, SE, MM',
-                'email'          => 'pejabat@admin.com',
-                'password'       => bcrypt('password'),
-                'remember_token' => null,
-                'username'       => 'DirSto2023',
-                'roleaccess'     => 1,
-            ],
-        ];
+	public function run()
+	{
+		$users = [
+			[
+				'id'             => 1,
+				'name'           => 'Admin',
+				'email'          => 'admin@admin.com',
+				'password'       => bcrypt('password'),
+				'remember_token' => null,
+				'username'       => 'admin',
+				'roleaccess'     => 1,
+			],
+			[
+				'id'             => 2,
+				'name'           => 'User1',
+				'email'          => 'user@user.com',
+				'password'       => bcrypt('password'),
+				'remember_token' => null,
+				'username'       => 'user1',
+				'roleaccess'     => 2,
+			],
+			[
+				'id'             => 3,
+				'name'           => 'Andi Muhammad Idil Fitri, SE, MM',
+				'email'          => 'pejabat@admin.com',
+				'password'       => bcrypt('password'),
+				'remember_token' => null,
+				'username'       => 'DirSto2023',
+				'roleaccess'     => 1,
+			],
+			[
+				'id'             => 4,
+				'name'           => 'SIAP RIPH',
+				'email'          => 'siapriph@email.com',
+				'password'       => bcrypt('siapriphsimethris'),
+				'remember_token' => null,
+				'username'       => 'siapriph',
+				'roleaccess'     => 2,
+			],
+		];
 
-        User::insert($users);
-    }
+		User::insert($users);
+	}
 }
