858c58fd98424a8635019da6b21c44db08dbdb36
RickySBahaso <ricky@sali-evo.com>
2023-09-24 18:30:44 +0700

    cek ini

--- app/Jobs/getkabprop.php
+++ app/Jobs/getkabprop.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class getkabprop implements ShouldQueue
@@ -35,17 +36,32 @@ class getkabprop implements ShouldQueue
     public function handle()
     {
         $filepath = 'master/token.json';
+        $token = null;
+        while (!Storage::disk('local')->exists($filepath))
+        {
+            //Log::debug('get-token');
+            $response = Http::asForm()->post(config('app.simevi_url').'getToken', [
+                'username' => config('app.simevi_user'),
+                'password' => config('app.simevi_pwd')
+            ]);
+            //Log::debug($response->json());
+            $filepath = 'master/token.json';
+            if (Storage::disk('local')->exists($filepath)) 
+                Storage::disk('local')->delete($filepath); 
+            Storage::disk('local')->put($filepath, json_encode($response->json()));
+            // $job = new gettoken();
+            // $this->dispatch($job);
+            // if (Storage::disk('local')->exists($filepath)) {
+            //     $pathjson = Storage::disk('local')->path($filepath);
+            //     $token = json_decode(file_get_contents($pathjson), true);
+            // }
+            
+        }
+
         if (Storage::disk('local')->exists($filepath)) {
             $pathjson = Storage::disk('local')->path($filepath);
             $token = json_decode(file_get_contents($pathjson), true);
-        } else {
-            $job = new gettoken();
-            $this->dispatch($job);
-            if (Storage::disk('local')->exists($filepath)) {
-                $pathjson = Storage::disk('local')->path($filepath);
-                $token = json_decode(file_get_contents($pathjson), true);
-            }
-        }
+        } 
 
         $response = Http::withToken($token['access_token'])->withHeaders([
             'Accept' => 'application/json'
