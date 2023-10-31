caa6e2a0ee44fcc280913b2324af78630587f487
sultanjawer <sultanjawer@gmail.com>
2023-10-31 16:45:54 +0700

    verification summary

--- app/Http/Controllers/Verifikator/VerifSklController.php
+++ app/Http/Controllers/Verifikator/VerifSklController.php
@@ -87,7 +87,7 @@ class VerifSklController extends Controller
 		if ($verifTanam) {
 			//ringkasan pengajuan verifikasi tanam\
 			$tanam = [
-				'avtDate' => optional($verifTanam)->created_at->format('d-m-Y') ?? '',
+				'avtDate' => optional($verifTanam)->created_at->format('d-m-Y'),
 				'avtVerifAt' => Carbon::parse(optional($verifTanam)->verif_at)->format('d-m-Y'),
 				'avtStatus' => optional($verifTanam)->status,
 				'avtMetode' => optional($verifTanam)->metode,
